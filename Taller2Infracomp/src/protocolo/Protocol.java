fb44eb7eb1f2bde22550f15f07ce8c061db07052
JuanMa Lovera <JuanMa Lovera>
2017-03-27 08:24:24 -0500

    Definición del protocolo

--- Taller2Infracomp/src/protocolo/Protocol.java
+++ Taller2Infracomp/src/protocolo/Protocol.java
@@ -1,5 +1,48 @@
 package protocolo;
 
 public class Protocol {
-
+	
+	//---------------------------------------
+	//Comandos del protocolo
+	//---------------------------------------
+	
+	public static final String HOLA = "HOLA";
+	
+	public static final String ACK = "ACK";
+	
+	public static final String ALGORITMOS = "ALGORITMOS";
+	
+	public static final String STATUS = "STATUS";
+	
+	public static final String OK = "OK";
+	
+	public static final String ERROR = "ERROR";
+	
+	public static final String CERTSRV = "CERTSRV";
+	
+	public static final String CERTCLNT = "CERTCLNT";
+	
+	public static final String INIT = "INIT";
+	
+	public static final String INFO = "INFO";
+	
+	//---------------------------------------
+	//Cadenas de caracteres para algoritmos
+	//---------------------------------------
+	
+	public static final String DES = "DES";
+	
+	public static final String AES = "AES";
+	
+	public static final String BLOWFISH = "Blowfish";
+	
+	public static final String RC4 = "RC4";
+	
+	public static final String RSA = "RSA";
+	
+	public static final String HMACMD5 = "HMACMD5";
+	
+	public static final String HMACSHA1 = "HMACSHA1";
+	
+	public static final String HMACSHA256 = "HMACSHA256";
 }
