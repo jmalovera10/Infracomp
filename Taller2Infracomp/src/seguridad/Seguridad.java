3c6be278ae28b1f1e4588fb79fed9aaa9022cb1a
JuanMa Lovera <jma.lovera10@uniandes.edu.co>
2017-03-30 18:19:22 -0500

    segunda aproximación CD

--- Taller2Infracomp/src/seguridad/Seguridad.java
+++ Taller2Infracomp/src/seguridad/Seguridad.java
@@ -1,27 +1,26 @@
 package seguridad;
 
 import java.math.BigInteger;
-import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
-import java.security.SignatureException;
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
-import java.util.Calendar;
 import java.util.Date;
 
-import javax.security.auth.x500.X500Principal;
-
-import org.bouncycastle.x509.X509V3CertificateGenerator;
+import org.bouncycastle.asn1.x500.X500NameBuilder;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 public class Seguridad {
-	
+
 	public X509Certificate generarCertificado(KeyPair keyPair)throws Exception{
-		Date startDate = new Date();
+		/*Date startDate = new Date();
 		Calendar calendar = Calendar.getInstance();
 		calendar.add(Calendar.DAY_OF_YEAR, 1);
 		Date expiryDate = calendar.getTime();             
@@ -34,27 +33,60 @@ public class Seguridad {
 		certGen.setNotAfter(expiryDate);
 		certGen.setSubjectDN(dnName);                       // note: same as issuer
 		certGen.setPublicKey(keyPair.getPublic());
+		certGen.setSignatureAlgorithm("SHA1WITHRSA");*/
+
+		/*Date startDate = new Date();                // time from which certificate is valid
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_YEAR, 1);
+		Date expiryDate = calendar.getTime();             // time after which certificate is not valid
+		BigInteger serialNumber = new BigInteger(""+Math.abs(SecureRandom.getInstance("SHA1PRNG").nextLong()));     // serial number for certificate 
+		PrivateKey caKey = keyPair.getPrivate();              // private key of the certifying authority (ca) certificate
+		//X509Certificate caCert =     // public key certificate of the certifying authority              // public/private key pair that we are creating certificate for
+		X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+		X500Principal  subjectName = new X500Principal("CN=Test V3 Certificate");
+
+		certGen.setSerialNumber(serialNumber);
+		certGen.setIssuerDN(caCert.getSubjectX500Principal());
+		certGen.setNotBefore(startDate);
+		certGen.setNotAfter(expiryDate);
+		certGen.setSubjectDN(subjectName);
+		certGen.setPublicKey(keyPair.getPublic());
 		certGen.setSignatureAlgorithm("SHA1WITHRSA");
 
-		return certGen.generate(keyPair.getPrivate());
-		
+		certGen.addExtension(X509Extensions.AuthorityKeyIdentifier, false, new AuthorityKeyIdentifierStructure(caCert));
+		certGen.addExtension(X509Extensions.SubjectKeyIdentifier, false,new SubjectKeyIdentifierStructure(keyPair.getPublic()));
+
+		X509Certificate cert = certGen.generate(caKey, "BC"); */
+
+		//Aproximación de http://programtalk.com/java-api-usage-examples/org.bouncycastle.cert.X509v3CertificateBuilder/
+		X500NameBuilder nameBuilder = new X500NameBuilder(BCStyle.INSTANCE);
+		nameBuilder.addRDN(BCStyle.CN, "localhost");
+		Date notBefore = new Date();
+		Date notAfter = new Date(System.currentTimeMillis() + 24 * 3 * 60 * 60 * 1000);
+		BigInteger serialNumber = new BigInteger(""+Math.abs(SecureRandom.getInstance("SHA1PRNG").nextLong()));
+		X509v3CertificateBuilder certificateBuilder = new JcaX509v3CertificateBuilder(nameBuilder.build(), serialNumber, notBefore, notAfter, nameBuilder.build(), keyPair.getPublic());
+		ContentSigner contentSigner = new JcaContentSignerBuilder("SHA256WithRSAEncryption").build(keyPair.getPrivate());
+		X509Certificate certificate = new JcaX509CertificateConverter().getCertificate(certificateBuilder.build(contentSigner));
+		return certificate;
+
 	}
-	
+
+
 	public void cifrarSimetrica(String obj, Key key, String algo ){
-		
+
 	}
-	
+
 	public void decifrarSimetrica(String obj, Key key, String algo){
-		
+
 	}
-	
+
 	public void cifrarAsimetrica(String obj, PublicKey key ){
-		
+
 	}
-	
+
 	public void decifrarAsimetrica(String obj, PrivateKey key){
-		
+
 	}
-		
+
 
 }
